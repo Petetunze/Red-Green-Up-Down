eed5538fb6e2ea4361c7882571f92217f0635a7a
Sebb <89808917+Petetunze@users.noreply.github.com>
2023-12-20 15:20:53 -0600

    Create RedGreenUpDown.swift

---
+++ RedGreenUpDown.swift
@@ -0,0 +1,17 @@
+//
+//  RedGreenUpDownApp.swift
+//  RedGreenUpDown
+//
+//  Created by Sebastian on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RedGreenUpDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
